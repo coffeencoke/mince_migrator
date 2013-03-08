f2375655b649d96e843af57d12c76f1ae21a534f
Matt Simpson <matt.simpson3@gmail.com>
2013-03-08 14:03:24 -0600

    Use root File class

--- spec/units/mince_migrator/deleter_spec.rb
+++ spec/units/mince_migrator/deleter_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../../lib/mince_migrator/deleter'
 
 describe MinceMigrator::Deleter do
   let(:name) { "name of migration" }
-  let(:migration_path) { File.join(path, "name_of_migration.rb") }
+  let(:migration_path) { ::File.join(path, "name_of_migration.rb") }
   let(:path) { mock }
   
   subject { described_class.new(name) }
@@ -13,7 +13,7 @@ describe MinceMigrator::Deleter do
 
   context 'when the migration does not exist' do
     before do
-      File.stub(:exists?).with(migration_path).and_return(false)
+      ::File.stub(:exists?).with(migration_path).and_return(false)
     end
 
     its(:can_delete_migration?) { should be_false }
@@ -21,7 +21,7 @@ describe MinceMigrator::Deleter do
 
   context 'when the migration exists' do
     before do
-      File.stub(:exists?).with(migration_path).and_return(true)
+      ::File.stub(:exists?).with(migration_path).and_return(true)
     end
 
     its(:can_delete_migration?) { should be_true }
