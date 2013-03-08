e6ad4b91d7ea7752602b1c4629cbf1610bed019b
Matt Simpson <matt.simpson3@gmail.com>
2013-03-07 21:00:37 -0600

    Remove mince related code in the integration spec for creating a migraiton

--- spec/integration/create_a_migration_spec.rb
+++ spec/integration/create_a_migration_spec.rb
@@ -1,18 +1,10 @@
 require_relative '../../lib/mince_migrator'
 
-require 'hashy_db'
-
-Mince::Config.interface = Mince::HashyDb::Interface
-
 describe "The creation of a migration:" do
-  subject { MinceMigrator::Creator.new options }
+  subject { MinceMigrator::Creator.new name }
 
-  after do
-    Mince::Config.interface.clear
-  end
-
-  describe "Attempting to create a migration without a name" do
-    let(:options) { nil }
+  context "when a name is not provided" do
+    let(:name) { nil }
 
     it "returns reasons why a migration could not be created" do
       subject.can_create_migration?.should be_false
@@ -20,7 +12,7 @@ describe "The creation of a migration:" do
   end
 
   describe "Creating a mince migration with a name" do
-    let(:options) { "Create seeded admin users" }
+    let(:name) { "Create seeded admin users" }
 
     it "can create the migration" do
       subject.can_create_migration?.should be_true
